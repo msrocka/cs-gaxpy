66b534d03193cde9a0951586d7fc701bbdabc437
msrocka <michael.srocka@gmail.com>
2022-02-01 15:20:21 +0100

    doc results

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # cs-gaxpy
+
+Calling out for a sparse matrix vector multiplication into Rust function via
+JNI. Because there is more JNI overhead than performance gain, it is not faster
+than the pure Java function that is used in openLCA currently:
+
+```
+iteration | Java [ms] | JNI+Rust [ms]
+0         | 0.775     | 3.220
+1         | 0.671     | 3.138
+2         | 0.668     | 3.142
+3         | 0.635     | 3.143
+4         | 0.632     | 3.045
+5         | 0.652     | 2.993
+6         | 0.662     | 3.078
+7         | 0.665     | 3.413
+8         | 0.642     | 3.130
+9         | 0.666     | 3.133
+```
--- src/test/java/org/openlca/cs/gaxpy/Benchmark.java
+++ src/test/java/org/openlca/cs/gaxpy/Benchmark.java
@@ -1,8 +1,45 @@
 package org.openlca.cs.gaxpy;
 
+import java.io.File;
+
+import org.openlca.core.DataDir;
+import org.openlca.core.library.LibMatrix;
+import org.openlca.core.matrix.format.CSCMatrix;
+
 public class Benchmark {
 
 	public static void main(String[] args) {
-		System.out.println("Works!");
+		System.out.println("  load native library");
+		CsGaxpy.loadFromFolder(new File("target"));
+
+		System.out.println("  load library data");
+		var lib = DataDir.getLibrary("en15804_00.00.001");
+		var s = lib.getColumn(LibMatrix.INV, 42).get();
+		var rawB = lib.getMatrix(LibMatrix.B).get();
+		var matrixB = rawB instanceof CSCMatrix csc
+			? csc
+			: CSCMatrix.of(rawB);
+
+		System.out.println("  check first result");
+		var javaResult = matrixB.multiply(s);
+		var nativeResult = CsGaxpy.mul(matrixB, s);
+		double diff = 0.0;
+		for (int i = 0; i < javaResult.length; i++) {
+			diff += Math.abs(javaResult[i] - nativeResult[i]);
+		}
+		System.out.println("  diff = " + diff);
+
+		System.out.println("iteration | Java [ms] | JNI+Rust [ms]");
+		for (int i = 0; i < 10; i++) {
+			long rustStart = System.nanoTime();
+			CsGaxpy.mul(matrixB, s);
+			double rustTime = ((double)(System.nanoTime() - rustStart)) / 1e6;
+
+			long javaStart = System.nanoTime();
+			matrixB.multiply(s);
+			double javaTime = ((double)(System.nanoTime() - javaStart)) / 1e6;
+			System.out.printf("%d         | %.3f     | %.3f%n", i, javaTime, rustTime);
+		}
+
 	}
 }
